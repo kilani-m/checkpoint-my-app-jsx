b8c880f199d98782cac24010847a5b443344898d
marwa-kilani <kilani.marwa93@gmail.com>
2022-05-09 12:28:52 +0100

    adding checkpoint-my-app-jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import img3 from './imageInSrc.png';
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div style={{border: 'solid 1px black', maxWidth: '100vw'}} className='App-header'>
+        <h1 className="title red">Your name here</h1>
+        <br />
+        <img src={img3} className='img1-src' alt="img1"/>
+        <br />
+        <img src="./imageInPublic.png" className='img2-src' alt="img2" />
+      </div>
+      <video width={320} height={240} controls>
+        <source src="myVideo.mp4" type="video/mp4" />
+      </video>
     </div>
   );
 }
-
 export default App;
